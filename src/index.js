f3cdeaf20c0d05ff16c3c0d0d6cfa183e3fefff8
Maryna Kalenska <kalenskaya.maryna@gmail.com>
2019-10-19 14:45:38 +0300

    Naive component approach

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
   return (
-    <div>
-      Hi there
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">
+            Nice post
+          </div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">
+            Nice post
+          </div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">
+            Nice post
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
